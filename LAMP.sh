0dad455c9a6a6d92104e5ce27988a38ecd91ea3b
tunguska55 <quintanillajon@gmail.com>
2018-02-06 15:38:50 -0500

    adding lamp stack without wordpress for quick set ups

---
+++ LAMP.sh
@@ -0,0 +1,6 @@
+#!/bin/bash/
+sudo yum install -y httpd mariadb-server mariadb php php-fpm php-mysql php-gd wget
+sudo systemctl start httpd
+sudo systemctl start mariadb
+sudo systemctl enable httpd
+sudo systemctl enable mariadb
