5cafffaebf0f8d2df52abd7a943ef12b704acbcb
tunguska55 <quintanillajon@gmail.com>
2018-02-21 15:26:34 -0500

    These are prerequisites for install Python 3. This should make it a little faster to get all of the yum installs out the way.

---
+++ Python Prereqs.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo yum groupinstall "Development tools" -y
+sudo yum install -y libffi-devel zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel expat-devel
