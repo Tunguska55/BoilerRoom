0e7ecb07129fb55764589b9d994b7f6cd7e6b045
tunguska55 <quintanillajon@gmail.com>
2018-02-26 13:04:59 -0500

    file removed and renamed to pythonPrereqs.sh

--- Python Prereqs.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sudo yum groupinstall "Development tools" -y
-sudo yum install -y libffi-devel zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel expat-devel
