3ec3bc36271ed0b144da589badc0515aeb925131
tunguska55 <quintanillajon@gmail.com>
2018-04-09 16:34:16 -0400

    modified the shebang for portability, added more sudo lines, added mysql secure install to get a start on that

--- LAMP-with-Wordpress.sh
+++ LAMP-with-Wordpress.sh
@@ -1,12 +1,14 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 sudo yum install -y httpd mariadb-server mariadb php php-fpm php-mysql php-gd wget
-sudo systemctl start httpd
-sudo systemctl start mariadb
-sudo systemctl enable httpd
-sudo systemctl enable mariadb
-wget http://wordpress.org/latest.tar.gz
-tar xzvf latest.tar.gz
-sudo rsync -avP ~/wordpress/ /var/www/html/
+sudo systemctl start httpd mariadb
+#sudo systemctl start mariadb
+sudo systemctl enable httpd mariadb
+#sudo systemctl enable mariadb
+cd /opt/
+sudo wget http://wordpress.org/latest.tar.gz
+sudo tar xzvf latest.tar.gz
+sudo rsync -avP ./wordpress/ /var/www/html/
 sudo mkdir /var/www/html/wp-content/uploads
 sudo chown -R apache:apache /var/www/html/*
 cp -v /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+mysql_secure_installation
